86e03d4c1fce85a52c90c538a2c75b47705d101b
Gustavo Matos Lázaro <39174920+Gubiar@users.noreply.github.com>
2019-09-25 09:09:33 -0300

    Create registro_struct_arquivo.c

---
+++ registro_struct_arquivo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
+
+
+typedef struct{
+  char *nome;
+  char *endereco;
+  int idade;
+  char *curso;
+}Aluno;
+
+int main(){
+
+ Aluno cadastro[1];
+ char buffer[100];
+
+    for (size_t i = 0; i < 1 ; ++i) {
+
+        FILE *arq;
+        arq = fopen("saida.txt", "w");
+
+        if(arq == NULL){
+            return false;
+        }
+
+        printf("Digite o nome do Aluno: \n");
+        scanf("%[^\n]s", buffer);
+        cadastro->nome = (char*) malloc(sizeof(char)*strlen(buffer));
+        strcpy(cadastro[i].nome, buffer);
+        __fpurge(stdin);
+
+        printf("Digite a idade do Aluno: \n");
+        scanf("%i", &cadastro[i].idade);
+        __fpurge(stdin);
+
+        printf("Digite o endereço do Aluno: \n");
+        scanf("%[^\n]s", buffer);
+        cadastro->endereco = (char*) malloc(sizeof(char)*strlen(buffer));
+        strcpy(cadastro[i].endereco, buffer);
+        __fpurge(stdin);
+
+        printf("Digite o curso do Aluno: \n");
+        scanf("%[^\n]s", buffer);
+        cadastro->curso = (char*) malloc(sizeof(char)*strlen(buffer));
+        strcpy(cadastro[i].curso, buffer);
+        __fpurge(stdin);
+
+        fprintf(arq, "%-50.50s|", cadastro[i].nome);
+        fprintf(arq, "%-5i|", cadastro[i].idade);
+        fprintf(arq, "%-50.50s|", cadastro[i].endereco);
+        fprintf(arq, "%-50.50s|", cadastro[i].curso);
+        fprintf(arq, "\n");
+
+        fflush(arq);
+        fclose(arq);
+
+    }
+
+
+
+return 0;
+}
