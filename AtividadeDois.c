75ccb9aafd2cc0e8962db0200206ebd5969fa76d
Gustavo Matos Lázaro <39174920+Gubiar@users.noreply.github.com>
2019-10-25 11:38:52 -0300

    Update AtividadeDois.c

--- AtividadeDois.c
+++ AtividadeDois.c
@@ -1,18 +1,58 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdio.h>
 
 typedef struct{
-
-    int id;
-    int rrn;
-
+  int id;
+  int rrn;
 }Registro;
+
+int particiona(Registro *V, int inicio, int fim){
+
+    int esquerda = inicio;
+    int direita = fim;
+    int pivo = V[inicio].id;
+
+    while(esquerda<=direita){
+
+        while(V[esquerda].id <= pivo){
+        esquerda++;
+        }
+
+        while(V[direita].id > pivo){
+            direita--;
+        }
+
+        if(esquerda < direita){
+
+            Registro aux = V[esquerda];
+            V[esquerda] = V[direita];
+            V[direita] = aux;
+        }
+    }
+
+    Registro aux =V[inicio];
+    V[inicio] = V[direita];
+    V[direita] = aux;
+
+    return direita;
+}
+
+void quicksort(Registro *V, int inicio, int fim){
+    int pivo;
+
+    if(inicio < fim){
+        pivo = particiona(V, inicio, fim);
+        quicksort(V,inicio, pivo-1);
+        quicksort(V, pivo+1, fim);
+    }
+
+} // função particiona
+
+
 
 int main(){
 
-    char buffer[5];
+char buffer[5];
 
     int tamanho_vetor;
 
@@ -25,7 +65,7 @@ int main(){
     tamanho_vetor = tamanho_vetor/56;
     fseek(arq,0,SEEK_SET);
 
-    Registro vetor[tamanho_vetor+1];
+    Registro vetor[tamanho_vetor];
 
     for(int i =0; i<tamanho_vetor; i++){
 
@@ -35,11 +75,14 @@ int main(){
           fseek(arq, 52, SEEK_CUR);
     }
 
-/*
+    quicksort(vetor,0,tamanho_vetor-1);
+
     for (size_t j = 0; j <tamanho_vetor ; j++) {
         printf("ID: %i\t", vetor[j].id );
         printf("RRN: %i\n", vetor[j].rrn );
     }
-*/
-    return 0;
+
+    
+
+  return 0;
 }
